f7e3f39925f24d2ad152668d9893276a079d5d40
aliwoto <aminnimaj@gmail.com>
2022-02-02 11:11:50 +0100

    Add more workflows and update date
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- ratelimiter/types.go
+++ ratelimiter/types.go
@@ -1,5 +1,5 @@
 // ratelimiter Project
-// Copyright (C) 2021 ALiwoto and other Contributors
+// Copyright (C) 2021~2022 ALiwoto and other Contributors
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of the source code.
 
